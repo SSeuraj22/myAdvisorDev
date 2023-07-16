90df6c09ba75b45b61a110e356bc667a5cb6258d
datalegacy <datalegacy@sundaebytes.com>
2023-07-16 22:17:42 +0000

    Revert "message"
    This reverts commit ffa36c427a201c329c8cc0c19886ace0465512f0.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const multer  = require('multer')
 const upload = multer({storage: multer.memoryStorage()})
 const { parse } = require('./utilities/parser');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // app connection and resources
 app.use(cors());
